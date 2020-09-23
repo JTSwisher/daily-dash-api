b72ef8d773496457561e90ab1f0f705f6443cc64
JTSwisher <jefftswisher@gmail.com>
2020-09-22 22:00:47 -0700

    fix routes.rb missing expected end of input

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
       namespace :v1 do
         resources :users, only: [:create] do 
           resources :todos, only: [:index, :create, :update, :destroy]
+        end 
+      end 
+    end 
 end
