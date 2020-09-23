65cc67050d0944072b72a55271c0487bbba51b9c
JTSwisher <jefftswisher@gmail.com>
2020-09-22 20:37:04 -0700

    add api v1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+    namespace :api do
+      namespace :v1 do
+        resources :users, only: [:create] do 
+          resources :todos, only: [:index, :create, :update, :destroy]
 end
