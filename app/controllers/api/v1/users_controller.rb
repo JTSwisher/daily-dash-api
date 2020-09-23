d4ec5521915c03c9575888c0e5a5118598bb50c1
JTSwisher <jefftswisher@gmail.com>
2020-09-23 16:18:32 -0700

    add create action to UsersController, add strong aprams user_params method to UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,26 @@
 class Api::V1::UsersController < ApplicationController
 
     def create
-
+        user = User.new(user_params)
+        if user.save
+            render json: user, only: [:id, :username, :name]
+        else 
+            render json: {
+                error: "Error signing up, please try again.",
+                status: 400
+              }, status: 400
+        end 
     end 
 
 # name - string
 #username = string
 #email - string 
 #password_digest - string
+
+private
+
+    def user_parms
+        params.require(:user).permit(:username, :name, :email, :password)
+    end 
 
 end 
