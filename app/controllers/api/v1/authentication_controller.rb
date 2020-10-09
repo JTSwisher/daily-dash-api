78ead6762a40dcb2bd7bc8f79b1bcce6bd8503be
JTSwisher <jefftswisher@gmail.com>
2020-10-08 18:13:05 -0700

    changelogin action in AuthenticationController

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,25 +2,32 @@ class Api::V1::AuthenticationController < ApplicationController
     
     def login
         user = User.find_by(username: user_params[:username])
-        if (!user)
-            render json: {error: "No such user exists."}
+        if (!user) 
+            render json: {
+                error: "No such user exists",
+                status: 400
+              }, status: 400
+        else 
+            if user.authenticate(user_params[:password])
+                secret_key = Rails.application.secrets.secret_key_base[0]
+                token = JWT.encode({
+                user_id: user.id,
+                username: user.username    
+                }, secret_key)
+                render json: { token: token, user: user }
+           else 
+                render json: {
+                    error: "Error signing in, please try again.",
+                    status: 400
+                  }, status: 400
+           end 
         end 
-       if user.authenticate(user_params[:password])
-            secret_key = Rails.application.secrets.secret_key_base[0]
-            token = JWT.encode({
-            user_id: user.id,
-            username: user.username    
-            }, secret_key)
-            render json: { token: token, user: user }
-       else 
-            render json: {error: "Invalid Password"}
-       end 
     end 
 
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :name, :password)
     end 
 
 end 
